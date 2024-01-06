0b9e24d1ba855d47841e14585deee1069ffc6cef
Mr.Thang <vothang226@gmail.com>
2024-01-06 23:57:19 +0700

    add

--- src/pages/ScanPage.jsx
+++ src/pages/ScanPage.jsx
@@ -1,86 +1,11 @@
-// import styles from "./stylesPage/ScanPage.module.scss";
-// import { useState, useEffect } from "react";
-// import QrScanner from "react-qr-scanner";
-// import axiosClient from "../libraries/axiosClient";
-// const ScanPage = () => {
-//   const [result, setResult] = useState("");
-//   const [productId, setProductId] = useState(null);
-//   const [detailProduct, setDetailProduct] = useState(null);
-
-//   const handleScan = (data) => {
-//     if (data) {
-//       setResult(data);
-//       try {
-//         const decodedData = JSON.parse(data.text);
-//         setProductId(decodedData.productId);
-//       } catch (error) {
-//         console.error("Error decoding QR code data:", error);
-//       }
-//     }
-//   };
-
-//   const handleError = (error) => {
-//     console.error("Error while scanning QR code:", error);
-//   };
-
-//   useEffect(() => {
-//     const getDetailProduct = async () => {
-//       if (productId) {
-//         try {
-//           const res = await axiosClient.get(`/products/${productId}`);
-//           setDetailProduct(res.data.payload || null);
-//           console.log("««««« detailProduct »»»»»", detailProduct);
-//         } catch (error) {
-//           console.error(error);
-//         }
-//       }
-//     };
-
-//     getDetailProduct();
-//   }, [productId]);
-
-//   return (
-//     <main className="container">
-//       <div className={styles.content}>
-//         <div className={styles.box_left}>
-//           <QrScanner
-//             onScan={handleScan}
-//             onError={handleError}
-//             style={{ width: "500px" }}
-//             facingMode="environment" // Mặc định mở camera sau
-//           />
-//           <p>{result && result.text}</p>
-//         </div>
-
-//         <div className={styles.box_right}>
-//           <h3>Chi tiết sản phẩm</h3>
-//           {detailProduct && (
-//             <>
-//               <p>ID: {detailProduct._id}</p>
-//               <p>Name: {detailProduct.name}</p>
-//               <p>Giá: {detailProduct.price}</p>
-//               <p>Giảm giá: {detailProduct.discount}</p>
-//               {/* Thêm các thông tin khác cần hiển thị */}
-//             </>
-//           )}
-//         </div>
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default ScanPage;
-
 import styles from "./stylesPage/ScanPage.module.scss";
 import { useState, useEffect } from "react";
 import QrScanner from "react-qr-scanner";
 import axiosClient from "../libraries/axiosClient";
-
 const ScanPage = () => {
   const [result, setResult] = useState("");
   const [productId, setProductId] = useState(null);
   const [detailProduct, setDetailProduct] = useState(null);
-  const [useFrontCamera, setUseFrontCamera] = useState(false);
 
   const handleScan = (data) => {
     if (data) {
@@ -96,10 +21,6 @@ const ScanPage = () => {
 
   const handleError = (error) => {
     console.error("Error while scanning QR code:", error);
-  };
-
-  const toggleCamera = () => {
-    setUseFrontCamera((prev) => !prev);
   };
 
   useEffect(() => {
@@ -127,7 +48,7 @@ const ScanPage = () => {
             onScan={handleScan}
             onError={handleError}
             style={{ width: "500px" }}
-            chooseDeviceId={useFrontCamera ? "front " : "rear"} // Chọn chế độ camera dựa trên trạng thái useFrontCamera
+            facingMode="rear" // Mặc định mở camera sau
           />
           <p>{result && result.text}</p>
         </div>
@@ -145,7 +66,6 @@ const ScanPage = () => {
           )}
         </div>
       </div>
-      <button onClick={toggleCamera}>Chuyển đổi Camera</button>
     </main>
   );
 };
