e4f11a0bd364e42d834caac44270bcdf700f0bc0
Mr.Thang <vothang226@gmail.com>
2024-01-06 18:39:37 +0700

    add

--- src/pages/ScanPage.jsx
+++ src/pages/ScanPage.jsx
@@ -1,13 +1,22 @@
-// import styles from "./stylesPage/ScanPage.module.scss"
-import { useState } from "react";
+import styles from "./stylesPage/ScanPage.module.scss";
+import { useState, useEffect } from "react";
 import QrScanner from "react-qr-scanner";
+import axiosClient from "../libraries/axiosClient";
+
 const ScanPage = () => {
   const [result, setResult] = useState("");
+  const [productId, setProductId] = useState(null);
+  const [detailProduct, setDetailProduct] = useState(null);
 
   const handleScan = (data) => {
     if (data) {
       setResult(data);
-      console.log('««««« result »»»»»', result);
+      try {
+        const decodedData = JSON.parse(data.text);
+        setProductId(decodedData.productId);
+      } catch (error) {
+        console.error("Error decoding QR code data:", error);
+      }
     }
   };
 
@@ -15,15 +24,47 @@ const ScanPage = () => {
     console.error("Error while scanning QR code:", error);
   };
 
+  useEffect(() => {
+    const getDetailProduct = async () => {
+      if (productId) {
+        try {
+          const res = await axiosClient.get(`/products/${productId}`);
+          setDetailProduct(res.data.payload || null);
+          console.log('««««« detailProduct »»»»»', detailProduct);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+
+    getDetailProduct();
+  }, [productId]);
+
   return (
     <main className="container">
-      <QrScanner
-        onScan={handleScan}
-        onError={handleError}
-        style={{ width: "50%", aspectRatio: "1/1" }} // Thiết lập aspect ratio để tạo thành hình vuông
-        idealFacingMode={2} // Sử dụng 1 để chọn camera phía sau, 2 để chọn camera phía trước
-      />
-      <p>{result && result.text}</p>
+      <div className={styles.content}>
+        <div className={styles.box_left}>
+          <QrScanner
+            onScan={handleScan}
+            onError={handleError}
+            style={{ width: "500px" }}
+          />
+          <p>{result && result.text}</p>
+        </div>
+        
+        <div className={styles.box_right}>
+          <h3>Chi tiết sản phẩm</h3>
+          {detailProduct && (
+            <>
+              <p>ID: {detailProduct._id}</p>
+              <p>Name: {detailProduct.name}</p>
+              <p>Giá: {detailProduct.price}</p>
+              <p>Giảm giá: {detailProduct.discount}</p>
+              {/* Thêm các thông tin khác cần hiển thị */}
+            </>
+          )}
+        </div>
+      </div>
     </main>
   );
 };
