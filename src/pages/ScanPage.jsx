591da3c1dbbf827b5e1126be9a3ba1b4724bd3ac
Mr.Thang <vothang226@gmail.com>
2024-01-06 16:48:52 +0700

    upp

--- src/pages/ScanPage.jsx
+++ src/pages/ScanPage.jsx
@@ -1,9 +1,31 @@
-import styles from "./stylesPage/ScanPage.module.scss"
-
+// import styles from "./stylesPage/ScanPage.module.scss"
+import{ useState } from "react";
+import QrReader from "react-qr-reader";
 const ScanPage = () => {
-  return (
-    <div>ScanPage</div>
-  )
-}
+  const [result, setResult] = useState("");
 
-export default ScanPage
+  const handleScan = (data) => {
+    if (data) {
+      setResult(data);
+      // Bạn có thể thực hiện các xử lý với dữ liệu QR ở đây
+    }
+  };
+
+  const handleError = (error) => {
+    console.error("Error while scanning QR code:", error);
+  };
+
+  return (
+    <main className="container">
+      <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: "100%" }}
+      />
+      <p>Result: {result}</p>
+    </main>
+  );
+};
+
+export default ScanPage;
