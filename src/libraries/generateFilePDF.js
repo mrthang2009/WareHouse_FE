c451fe22193f4982724a032435031581270105d4
Mr.Thang <vothang226@gmail.com>
2024-01-12 17:16:36 +0700

    add

--- src/libraries/generateFilePDF.js
+++ src/libraries/generateFilePDF.js
@@ -1,59 +1,132 @@
-import numeral from "numeral";
+// import numeral from "numeral";
+// import "numeral/locales/vi";
+// numeral.locale("vi");
+// import "jspdf-autotable";
+// import html2pdf from "html2pdf.js";
+
+// const generateFilePDF = (data) => {
+//   const content = `
+//       <html>
+//         <head>
+//           <style>
+//           h1 {
+//             text-align: center;
+//           }
+//             table { width: 100%; text-align: center}
+//           </style>
+//         </head>
+//         <body>
+//           <h1>Danh sách sản phẩm</h1>
+//           <table>
+//             <thead>
+//               <tr>
+//                 <th>STT</th>
+//                 <th>Tên sản phẩm</th>
+//                 <th>Mã QR code</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               ${data
+//                 .map(
+//                   (item, index) => `
+//                     <tr>
+//                       <td>${index + 1}</td>
+//                       <td>${item.name}</td>
+//                       <td><img src="${item.media?.qrCodeUrl}" alt="QR Code">
+//                       <p>${item.name}</p></td>
+//                     </tr>
+//                   `
+//                 )
+//                 .join("")}
+//             </tbody>
+//           </table>
+//         </body>
+//       </html>
+//     `;
+//   const options = {
+//     margin: 10,
+//     filename: "Danh sách sản phẩm.pdf",
+//     image: { type: "image/png", quality: 1 },
+//     html2canvas: {
+//       scale: 2,
+//       useCORS: true, // Thêm tùy chọn này để buộc sử dụng CORS
+//     },
+//     jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+//   };
+
+//   html2pdf().set(options).from(content).save();
+// };
+// export default generateFilePDF;
+
 import "numeral/locales/vi";
-numeral.locale("vi");
 import "jspdf-autotable";
 import html2pdf from "html2pdf.js";
+import QRCode from "qrcode-generator";
 
 const generateFilePDF = (data) => {
   const content = `
-      <html>
-        <head>
-          <style>
+    <html>
+      <head>
+        <style>
           h1 {
             text-align: center;
           }
-            table { width: 100%; text-align: center}
-          </style>
-        </head>
-        <body>
-          <h1>Danh sách sản phẩm</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>STT</th>
-                <th>Tên sản phẩm</th>
-                <th>Mã QR code</th>
-              </tr>
-            </thead>
-            <tbody>
-              ${data
-                .map(
-                  (item, index) => `
+          table {
+            width: 100%;
+            text-align: center;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Danh sách sản phẩm</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>STT</th>
+              <th>Tên sản phẩm</th>
+              <th>Mã QR code</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${data
+              .map((item, index) => {
+                // Tạo mã QR
+                const qrCode = QRCode(0, "L");
+                const dataToEncode = `productId: ${item._id}`;
+                qrCode.addData(dataToEncode);
+                qrCode.make();
+                const qrDataURL = qrCode.createDataURL();
+
+                return `
                     <tr>
                       <td>${index + 1}</td>
                       <td>${item.name}</td>
-                      <td><img src="${item.media?.qrCodeUrl}" alt="QR Code">
-                      <p>${item.name}</p></td>
+                      <td>
+                        <img src="${qrDataURL}" alt="QR Code">
+                        <p>${item.name}</p>
+                      </td>
                     </tr>
-                  `
-                )
-                .join("")}
-            </tbody>
-          </table>
-        </body>
-      </html>
-    `;
+                  `;
+              })
+              .join("")}
+          </tbody>
+        </table>
+      </body>
+    </html>
+  `;
+
   const options = {
     margin: 10,
     filename: "Danh sách sản phẩm.pdf",
-    image: { type: "png", quality: 1 },
+    image: { type: "image/png", quality: 1 },
     html2canvas: {
       scale: 2,
-      useCORS: true, // Thêm tùy chọn này để buộc sử dụng CORS
+      useCORS: true,
     },
     jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
   };
 
-  html2pdf().from(content).set(options).save();
+  html2pdf().set(options).from(content).save();
 };
+
 export default generateFilePDF;
